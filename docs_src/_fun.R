e171cab9d6e605515447006b089448dfeada28d5
Nikolay Gniteev <godhart@gmail.com>
2021-09-21 10:11:27 +0300

    More options for diagram extension

--- docs_src/_fun.R
+++ docs_src/_fun.R
@@ -1,9 +1,15 @@
-to_diagram <- function(dia, name, data = "", src = "", inline = TRUE) {
-  if (knitr::is_latex_output()) {
-    # PDF for PDF, SVG for HTML
-    ext <- "pdf"
+to_diagram <- function(dia, name, data = "", src = "", inline = TRUE, ext = "", krk = "") {
+  if (krk == "") {    # If Kroki! extension is not specified - svg would be used
+    krk <- "svg"
+    if (ext == "") {  # If output extension is not specified - use defaults
+      if (knitr::is_latex_output()) {
+        ext <- "pdf"  # PDF for PDF
+      } else {
+        ext <- "svg"  # SVG for HTML
+      }
+    }
   } else {
-    ext <- "svg"
+    ext <- krk  # Otherwise outuput type should be same as for Kroki!
   }
   fname <- name # TODO: substitute prohibited chars like this: `gsub(" ", "-", name, fixed = TRUE)`
   if (src == "") {
@@ -19,11 +25,11 @@ to_diagram <- function(dia, name, data = "", src = "", inline = TRUE) {
   if (knitr::is_latex_output() || !inline) {
     # Render as Picture
     # TODO: str_interp
-    if (!file.exists(paste("'generated/",fname,".svg'", sep=""))) {
+    if (!file.exists(paste("'generated/",fname,".",krk,"'", sep=""))) {
       if (dia != "from_src" || src == "") {
         system2("curl", c(
-        paste("http://kroki:8000/",dia,"/svg", sep=""),
-        "-o", paste("'generated/",fname,".svg'", sep=""),
+        paste("http://kroki:8000/",dia,"/",krk, sep=""),
+        "-o", paste("'generated/",fname,".",krk,"'", sep=""),
         data_mode,
         data_value
         ),
@@ -33,28 +39,28 @@ to_diagram <- function(dia, name, data = "", src = "", inline = TRUE) {
                 paste("sed -n '/```\\sdiagram-/,/```/{/```\\sdiagram-/b;/```/b;p}' '",src,"'"     # Get content
                 , " | curl http://kroki:8000/"                              # Request to server
                 , "`cat '",src,"' | grep -oP '\\`\\`\\` diagram-\\K.*'`"    # Get diagram type (aka engine)
-                , "/svg"                                                    # Incoming data is always SVG
+                , "/",krk                                                   # Incoming data format
                 , " --data-binary @-"                                       # Send piped data in POST request
-                , " -o 'generated/",fname,".svg","'"                        # Output to file
+                , " -o 'generated/",fname,".",krk,"'"                       # Output to file
                 , sep="")
                 , ignore.stdout=TRUE, ignore.stderr=TRUE
         )
       }
     }
-    # Convert SVG to PDF
-    if (ext == 'pdf') {
+    # Convert SVG to necessary type
+    if (krk == "svg" && ext != "svg") {
         system(
-            paste("rsvg-convert -f pdf -o 'generated/",fname,".pdf' 'generated/",fname,".svg'"
+            paste("rsvg-convert -f ",ext," -o 'generated/",fname,".",ext,"' 'generated/",fname,".svg'"
             , sep="")
             , ignore.stdout=TRUE, ignore.stderr=TRUE
         )
     }
-    cat(paste("![", name, "](./generated/",fname,".",ext,")", sep=""))
+    cat(paste("![", name, "](./generated/",fname,".",ext,")", sep=""))  # TODO: ref, width, height
   } else {
     # Render Inline SVG for HTML
     if (dia != "from_src" || src == "") {
         cat(system2("curl", c(
-        paste("http://kroki:8000/",dia,"/",ext, sep=""),
+        paste("http://kroki:8000/",dia,"/",krk, sep=""),
         data_mode,
         data_value
         ),
@@ -65,7 +71,7 @@ to_diagram <- function(dia, name, data = "", src = "", inline = TRUE) {
                 paste("sed -n '/```\\sdiagram-/,/```/{/```\\sdiagram-/b;/```/b;p}' '",src,"'"     # Get content
                 , " | curl http://kroki:8000/"                              # Request to server
                 , "`cat '",src,"' | grep -oP '\\`\\`\\` diagram-\\K.*'`/"   # Get diagram type
-                , ext                                                       # Output data format (svg/png)
+                , krk                                                       # Output data format (svg/png)
                 , " --data-binary @-"                                       # Send piped data in POST request
                 , sep="")
                 ,intern=TRUE, ignore.stderr=TRUE
@@ -73,5 +79,6 @@ to_diagram <- function(dia, name, data = "", src = "", inline = TRUE) {
             sep=""
         )
     }
+    cat(paste("![", name, "](./images/hidden_pixel.png)", sep=""))  # TODO: ref, width, height
   }
 }
