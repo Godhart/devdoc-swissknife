1eb1c0221a23fd35347793f21326806261c74acd
Nikolay Gniteev <godhart@gmail.com>
2022-05-08 00:48:47 +0300

    [build] docs are rendered to `docs_out` from this moment

--- docs_src/render_doc.sh
+++ docs_src/render_doc.sh
@@ -5,8 +5,8 @@ rm -rf generated &&\
 mkdir generated &&\
 rm -rf _book &&\
 BOOKDOWN_FULL_PDF=false Rscript --quiet ../_render.R &&\
-rm -rf ../../out/$1 &&\
-mkdir ../../out/$1 &&\
+rm -rf ../../docs_out/$1 &&\
+mkdir ../../docs_out/$1 &&\
 sleep 10s &&\
-mv -f _book/* ../../out/$1 &&\
+mv -f _book/* ../../docs_out/$1 &&\
 rm -rf _book
