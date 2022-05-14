48facbf2e34c481c491efdcbc0c77c6be88b44f4
Nikolay Gniteev <godhart@gmail.com>
2022-05-14 06:25:08 +0300

    [build] temp dir rename `_book` => `.book`

--- docs_src/render_doc.sh
+++ docs_src/render_doc.sh
@@ -3,10 +3,9 @@
 rm -f \"`cat _bookdown.yml | grep -Po 'book_filename: "\K[^"#]*' | xargs`.Rmd\" &&\
 rm -rf generated &&\
 mkdir generated &&\
-rm -rf _book &&\
+rm -rf .book &&\
 BOOKDOWN_FULL_PDF=false Rscript --quiet ../_render.R &&\
 rm -rf ../../docs_out/$1 &&\
 mkdir ../../docs_out/$1 &&\
-sleep 10s &&\
-mv -f _book/* ../../docs_out/$1 &&\
-rm -rf _book
+mv -f .book/* ../../docs_out/$1 &&\
+rm -rf .book
